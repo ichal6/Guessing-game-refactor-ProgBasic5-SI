4da74126610a8f1f74bf521a28364267fef5d08f
Michael Lechowicz <ichal.lechowicz@gmail.com>
2019-08-27 22:44:06 +0200

    Complexity: Rename g to guess

--- guessing_game.py
+++ guessing_game.py
@@ -12,14 +12,14 @@ list_random_int.append(random.randint(1, 99))
 list_random_int.append(random.randint(1, 99))
 list_random_int.append(random.randint(1, 99))
 for index_of_number in range(COUNT_NUMBER_TO_GUESS):
-    g = int(input("Enter an integer from 1 to 99: "))
-    while list_random_int[index_of_number] != g:
-        if g < list_random_int[index_of_number]:
+    guess = int(input("Enter an integer from 1 to 99: "))
+    while list_random_int[index_of_number] != guess:
+        if guess < list_random_int[index_of_number]:
             print("guess is low")
-            g = int(input("Enter an integer from 1 to 99: "))
-        elif g > list_random_int[index_of_number]:
+            guess = int(input("Enter an integer from 1 to 99: "))
+        elif guess > list_random_int[index_of_number]:
             print("guess is high")
-            g = int(input("Enter an integer from 1 to 99: "))
+            guess = int(input("Enter an integer from 1 to 99: "))
         else:
             break
     print("you guessed it!")
@@ -36,14 +36,14 @@ list_random_int.append(random.randint(1, 49))
 list_random_int.append(random.randint(1, 49))
 list_random_int.append(random.randint(1, 49))
 for index_of_number in range(COUNT_NUMBER_TO_GUESS):
-    g = int(input("Enter an integer from 1 to 49: "))
-    while list_random_int[index_of_number] != g:
-        if g < list_random_int[index_of_number]:
+    guess = int(input("Enter an integer from 1 to 49: "))
+    while list_random_int[index_of_number] != guess:
+        if guess < list_random_int[index_of_number]:
             print("guess is low")
-            g = int(input("Enter an integer from 1 to 49: "))
-        elif g > list_random_int[index_of_number]:
+            guess = int(input("Enter an integer from 1 to 49: "))
+        elif guess > list_random_int[index_of_number]:
             print("guess is high")
-            g = int(input("Enter an integer from 1 to 49: "))
+            guess = int(input("Enter an integer from 1 to 49: "))
         else:
             break
     print("you guessed it!")

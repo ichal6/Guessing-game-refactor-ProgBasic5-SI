2ea67aa7ca3051f47ec90a71bbe516ba478f035a
Michael Lechowicz <ichal.lechowicz@gmail.com>
2019-08-27 22:58:49 +0200

    Duplication: Add while loop for append to list

--- guessing_game.py
+++ guessing_game.py
@@ -1,16 +1,12 @@
 import random
 COUNT_NUMBER_TO_GUESS = 10
+
 list_random_int = []
-list_random_int.append(random.randint(1, 99))
-list_random_int.append(random.randint(1, 99))
-list_random_int.append(random.randint(1, 99))
-list_random_int.append(random.randint(1, 99))
-list_random_int.append(random.randint(1, 99))
-list_random_int.append(random.randint(1, 99))
-list_random_int.append(random.randint(1, 99))
-list_random_int.append(random.randint(1, 99))
-list_random_int.append(random.randint(1, 99))
-list_random_int.append(random.randint(1, 99))
+iteration = 0
+while iteration < COUNT_NUMBER_TO_GUESS:
+    list_random_int.append(random.randint(1, 99))
+    iteration += 1
+
 for index_of_number in range(COUNT_NUMBER_TO_GUESS):
     guess = int(input("Enter an integer from 1 to 99: "))
     while list_random_int[index_of_number] != guess:
@@ -23,16 +19,10 @@ for index_of_number in range(COUNT_NUMBER_TO_GUESS):
     print("you guessed it!")
 
 list_random_int = []
-list_random_int.append(random.randint(1, 49))
-list_random_int.append(random.randint(1, 49))
-list_random_int.append(random.randint(1, 49))
-list_random_int.append(random.randint(1, 49))
-list_random_int.append(random.randint(1, 49))
-list_random_int.append(random.randint(1, 49))
-list_random_int.append(random.randint(1, 49))
-list_random_int.append(random.randint(1, 49))
-list_random_int.append(random.randint(1, 49))
-list_random_int.append(random.randint(1, 49))
+iteration = 0
+while iteration < COUNT_NUMBER_TO_GUESS:
+    list_random_int.append(random.randint(1, 49))
+    iteration += 1
 for index_of_number in range(COUNT_NUMBER_TO_GUESS):
     guess = int(input("Enter an integer from 1 to 49: "))
     while list_random_int[index_of_number] != guess:

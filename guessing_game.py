e28b8ccf3f38457eeeaab393f0cebe55a68d3e44
Michael Lechowicz <ichal.lechowicz@gmail.com>
2019-08-27 23:05:27 +0200

    Duplication: Add check_guess_number function

--- guessing_game.py
+++ guessing_game.py
@@ -1,35 +1,26 @@
 import random
 COUNT_NUMBER_TO_GUESS = 10
 
-list_random_int = []
-iteration = 0
-while iteration < COUNT_NUMBER_TO_GUESS:
-    list_random_int.append(random.randint(1, 99))
-    iteration += 1
 
-for index_of_number in range(COUNT_NUMBER_TO_GUESS):
-    guess = int(input("Enter an integer from 1 to 99: "))
-    while list_random_int[index_of_number] != guess:
-        if guess < list_random_int[index_of_number]:
-            print("guess is low")
-            guess = int(input("Enter an integer from 1 to 99: "))
-        elif guess > list_random_int[index_of_number]:
-            print("guess is high")
-            guess = int(input("Enter an integer from 1 to 99: "))
-    print("you guessed it!")
+def check_guess_number(end_of_range):
+    list_random_int = []
+    iteration = 0
+    while iteration < COUNT_NUMBER_TO_GUESS:
+        list_random_int.append(random.randint(1, end_of_range))
+        iteration += 1
 
-list_random_int = []
-iteration = 0
-while iteration < COUNT_NUMBER_TO_GUESS:
-    list_random_int.append(random.randint(1, 49))
-    iteration += 1
-for index_of_number in range(COUNT_NUMBER_TO_GUESS):
-    guess = int(input("Enter an integer from 1 to 49: "))
-    while list_random_int[index_of_number] != guess:
-        if guess < list_random_int[index_of_number]:
-            print("guess is low")
-            guess = int(input("Enter an integer from 1 to 49: "))
-        elif guess > list_random_int[index_of_number]:
-            print("guess is high")
-            guess = int(input("Enter an integer from 1 to 49: "))
-    print("you guessed it!")
+    command = "Enter an integer from 1 to {}: ".format(str(end_of_range))
+    for index_of_number in range(COUNT_NUMBER_TO_GUESS):
+        guess = int(input(command))
+        while list_random_int[index_of_number] != guess:
+            if guess < list_random_int[index_of_number]:
+                print("guess is low")
+                guess = int(input(command))
+            elif guess > list_random_int[index_of_number]:
+                print("guess is high")
+                guess = int(input(command))
+        print("you guessed it!")
+
+
+check_guess_number(99)
+check_guess_number(49)

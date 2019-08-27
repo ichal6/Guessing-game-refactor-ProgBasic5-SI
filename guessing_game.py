fc1348b5247fe4f88b10c3d6e845222e48df0689
Michael Lechowicz <ichal.lechowicz@gmail.com>
2019-08-27 22:35:23 +0200

    Complexity: Rename i to index_of_number

--- guessing_game.py
+++ guessing_game.py
@@ -10,13 +10,13 @@ list_random_int.append(random.randint(1, 99))
 list_random_int.append(random.randint(1, 99))
 list_random_int.append(random.randint(1, 99))
 list_random_int.append(random.randint(1, 99))
-for i in range(10):
+for index_of_number in range(10):
     g = int(input("Enter an integer from 1 to 99: "))
-    while list_random_int[i] != g:
-        if g < list_random_int[i]:
+    while list_random_int[index_of_number] != g:
+        if g < list_random_int[index_of_number]:
             print("guess is low")
             g = int(input("Enter an integer from 1 to 99: "))
-        elif g > list_random_int[i]:
+        elif g > list_random_int[index_of_number]:
             print("guess is high")
             g = int(input("Enter an integer from 1 to 99: "))
         else:
@@ -34,13 +34,13 @@ list_random_int.append(random.randint(1, 49))
 list_random_int.append(random.randint(1, 49))
 list_random_int.append(random.randint(1, 49))
 list_random_int.append(random.randint(1, 49))
-for i in range(10):
+for index_of_number in range(10):
     g = int(input("Enter an integer from 1 to 49: "))
-    while list_random_int[i] != g:
-        if g < list_random_int[i]:
+    while list_random_int[index_of_number] != g:
+        if g < list_random_int[index_of_number]:
             print("guess is low")
             g = int(input("Enter an integer from 1 to 49: "))
-        elif g > list_random_int[i]:
+        elif g > list_random_int[index_of_number]:
             print("guess is high")
             g = int(input("Enter an integer from 1 to 49: "))
         else:
